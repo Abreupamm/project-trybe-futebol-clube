d4bf4cd4f24652ada7194c97f715e9f3d298cea7
Pamela <abrepamm@gmail.com>
2022-10-25 19:43:27 -0300

    feat: migration da tabela teams criada

--- app/backend/src/database/migrations/20221025222517-create-teams.js
+++ app/backend/src/database/migrations/20221025222517-create-teams.js
@@ -1,21 +1,20 @@
-'use strict';
-
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('teams', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      team_name: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+    });
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+    await queryInterface.dropTable('teams');
+  },
 };

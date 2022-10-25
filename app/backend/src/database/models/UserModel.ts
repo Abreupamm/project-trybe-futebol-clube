7072b8828d3e7caf08817c5629d0453e4d9ab181
Pamela <abrepamm@gmail.com>
2022-10-25 19:23:24 -0300

    feat: Model Users criado

---
+++ app/backend/src/database/models/UserModel.ts
@@ -0,0 +1,43 @@
+import { Model, STRING, INTEGER } from 'sequelize';
+import db from '.';
+// import OtherModel from './OtherModel';
+
+class Users extends Model {
+  declare id: number;
+  declare username: string;
+  declare role: string;
+  declare email: string;
+  declare password: string;
+}
+
+Users.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  username: {
+    type: STRING,
+    allowNull: false,
+  },
+  role: {
+    type: STRING,
+    allowNull: false,
+  },
+  email: {
+    type: STRING,
+    allowNull: false,
+  },
+  password: {
+    type: STRING,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'users',
+  timestamps: false,
+});
+
+export default Users;

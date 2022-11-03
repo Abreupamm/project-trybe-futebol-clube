a7e9771ef4c06af09a67c2f8de646bda806f07f2
Pamela <abrepamm@gmail.com>
2022-11-02 21:58:04 -0300

    validação de login adicionadas

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -30,38 +30,38 @@ describe('Verifica  o endpoint /login no back-end de maneira que ele permita o a
       expect(httpResponse.status).to.equal(200);
     });
    
-    // it('Verifica que não é possível fazer login sem informar o email', async () => {
-    //   const httpResponse = await chai
-    //     .request(app)
-    //     .post('/login')
-    //     .send({ email: '', password: 'secret_admin' });
-    //   expect(httpResponse.status).to.equal(400);
-    //   expect(httpResponse.body).to.deep.equal({
-    //     message: 'All fields must be filled',
-    //   });
-    // });
+    it('Verifica que não é possível fazer login sem informar o email', async () => {
+      const httpResponse = await chai
+        .request(app)
+        .post('/login')
+        .send({ email: '', password: 'secret_admin' });
+      expect(httpResponse.status).to.equal(400);
+      expect(httpResponse.body).to.deep.equal({
+        message: 'All fields must be filled',
+      });
+    });
 
-    // it('verifica que não é possível fazer login sem informar a senha', async () => {
-    //   const httpResponse = await chai
-    //     .request(app)
-    //     .post('/login')
-    //     .send({ email: 'maria@email.com', password: '' });
-    //   expect(httpResponse.status).to.equal(400);
-    //   expect(httpResponse.body).to.deep.equal({
-    //     message: 'All fields must be filled',
-    //   });
-    // });
+    it('verifica que não é possível fazer login sem informar a senha', async () => {
+      const httpResponse = await chai
+        .request(app)
+        .post('/login')
+        .send({ email: 'maria@email.com', password: '' });
+      expect(httpResponse.status).to.equal(400);
+      expect(httpResponse.body).to.deep.equal({
+        message: 'All fields must be filled',
+      });
+    });
 
-    // it('Verifica que não é possível fazer login com um email inválido', async () => {
-    //   const httpResponse = await chai
-    //     .request(app)
-    //     .post('/login')
-    //     .send({ email: '@test@.com', password: 'secret_admin' });
-    //   expect(httpResponse.status).to.equal(401);
-    //   expect(httpResponse.body).to.deep.equal({
-    //     message: 'Incorrect email or password',
-    //   });
-    // });
+    it('Verifica que não é possível fazer login com um email inválido', async () => {
+      const httpResponse = await chai
+        .request(app)
+        .post('/login')
+        .send({ email: '@test@.com', password: 'secret_admin' });
+      expect(httpResponse.status).to.equal(401);
+      expect(httpResponse.body).to.deep.equal({
+        message: 'Incorrect email or password',
+      });
+    });
 
     it('verifica que não é possível fazer login com uma senha inválida', async () => {
       const httpResponse = await chai
